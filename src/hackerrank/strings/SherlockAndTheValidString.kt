819a1eaac87cf13a4060683d0f226e77d582f580
Yevhen Troshchii <yvgen.troshchiy@gmail.com>
2019-01-08 20:06:47 +0200

    Add draft SherlockAndTheValidString

---
+++ src/hackerrank/strings/SherlockAndTheValidString.kt
@@ -0,0 +1,16 @@
+package hackerrank.strings
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val s = scan.nextLine()
+    val result = isValid(s)
+
+    println(result)
+}
+
+// Complete the isValid function below.
+fun isValid(s: String): String {
+    return "s"
+}
