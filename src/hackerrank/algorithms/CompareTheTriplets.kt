5828e7537d73d78b7478c9482f0f532c438fb263
Yevhen Troshchii <yvgen.troshchiy@gmail.com>
2019-01-08 18:29:19 +0200

    Add CompareTheTriplets

--- src/hackerrank/algorithms/CompareTheTriplets.kt
+++ src/hackerrank/algorithms/CompareTheTriplets.kt
@@ -1,16 +1,26 @@
 package hackerrank.algorithms
 
-// Complete the compareTriplets function below.
-fun compareTriplets(a: Array<Int>, b: Array<Int>): Array<Int> {
-return arrayOf(1,2)
-}
-
 fun main(args: Array<String>) {
-    val a = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+//    val a = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
+//    val b = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
 
-    val b = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    val a = arrayOf(5, 6, 7)
+    val b = arrayOf(3, 6, 10)
 
     val result = compareTriplets(a, b)
 
     println(result.joinToString(" "))
+}
+
+// Complete the compareTriplets function below.
+fun compareTriplets(a: Array<Int>, b: Array<Int>): Array<Int> {
+    var alicePoints = 0
+    var bobPoints = 0
+
+    for (i in 0..2) {
+        if (a[i] > b[i]) alicePoints++
+        else if (b[i] > a[i]) bobPoints++
+    }
+
+    return arrayOf(alicePoints, bobPoints)
 }
