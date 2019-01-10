43631ee045407a87f5c734c496c3df78eb3cd422
Yevhen Troshchii <yvgen.troshchiy@gmail.com>
2019-01-11 00:17:20 +0200

    Add SockMerchant

---
+++ src/hackerrank/warmup/SockMerchant.kt
@@ -0,0 +1,37 @@
+package hackerrank.warmup
+
+private val n = 9
+private val ar1 = arrayOf(10, 20, 20, 10, 10, 30, 50, 10, 20) // 3
+private val ar2 = arrayOf(1, 2, 1, 2, 1, 3, 2) // 2
+private val TC10 = arrayOf(1, 1, 3, 1, 2, 1, 3, 3, 3, 3) // 4
+
+//fun main(args: Array<String>) {
+//    val scan = Scanner(System.`in`)
+//    val n = scan.nextLine().trim().toInt()
+//    val ar = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+//    val result = sockMerchant(n, ar)
+//    println(result)
+//}
+
+fun main(args: Array<String>) {
+    val result = sockMerchant(n, TC10)
+    println(result)
+}
+
+// Complete the sockMerchant function below.
+fun sockMerchant(n: Int, ar: Array<Int>): Int {
+    var result = 0
+
+    val map = mutableMapOf<Int, Int>()
+
+    for (item: Int in ar) {
+        map[item] = (map[item] ?: 0) + 1
+    }
+
+    for ((key, value) in map) {
+        val i = value / 2
+        result += i
+    }
+
+    return result
+}
