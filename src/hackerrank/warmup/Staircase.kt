f461ffc5af8e2c76f75c5c9681d11ac83f765785
Yevhen Troshchii <yvgen.troshchiy@gmail.com>
2019-01-12 02:57:27 +0200

    Add staircase2

--- src/hackerrank/warmup/Staircase.kt
+++ src/hackerrank/warmup/Staircase.kt
@@ -1,7 +1,18 @@
 package hackerrank.warmup
 
 fun main(args: Array<String>) {
+
+    for (i in 0..0) {
+        println("dots")
+    }
+
+    for (i in 0 until 0) {
+        println("until")
+    }
+
     staircase(6)
+    println()
+    staircase2(6)
 }
 
 fun staircase(n: Int): Unit {
@@ -14,5 +25,18 @@ fun staircase(n: Int): Unit {
         }
 
         if (i < n) println()
+    }
+}
+
+fun staircase2(n: Int): Unit {
+    for (i in 0 until n) {
+        for (s in 0 until (n - 1) - i) {
+            print(" ")
+        }
+        for (l in 0..i) {
+            print("#")
+        }
+
+        if (i < n - 1) println()
     }
 }
