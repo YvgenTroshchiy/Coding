8c59e80db8de0b3a0cf2b4e295fd989f34df835d
Yevhen Troshchii <yvgen.troshchiy@gmail.com>
2019-01-16 02:20:54 +0200

    Add twoSum

--- src/leetcode/TwoSum.kt
+++ src/leetcode/TwoSum.kt
@@ -1,158 +1,38 @@
 package leetcode
 
-import java.util.*
 
+private val nums1 = intArrayOf(2, 7, 11, 15)
+private val t1 = 9 // [0,1]
 
-/*
-[230,863,916,585,981,404,316,785,88,12,70,435,384,778,887,755,740,337,86,92,325,422,815,650,920,125,277,336,221,847,168,23,677,61,400,136,874,363,394,199,863,997,794,587,124,321,212,957,764,173,314,422,927,783,930,282,306,506,44,926,691,568,68,730,933,737,531,180,414,751,28,546,60,371,493,370,527,387,43,541,13,457,328,227,652,365,430,803,59,858,538,427,583,368,375,173,809,896,370,789]
-542
-*/
-
-private val nums0 = intArrayOf(2, 7, 11, 15)
-private val t0 = 9 // [0, 1]
-
-private val nums1 = intArrayOf(
-    230,
-    863,
-    916,
-    585,
-    981,
-    404,
-    316,
-    785,
-    88,
-    12,
-    70,
-    435,
-    384,
-    778,
-    887,
-    755,
-    740,
-    337,
-    86,
-    92,
-    325,
-    422,
-    815,
-    650,
-    920,
-    125,
-    277,
-    336,
-    221,
-    847,
-    168,
-    23,
-    677,
-    61,
-    400,
-    136,
-    874,
-    363,
-    394,
-    199,
-    863,
-    997,
-    794,
-    587,
-    124,
-    321,
-    212,
-    957,
-    764,
-    173,
-    314,
-    422,
-    927,
-    783,
-    930,
-    282,
-    306,
-    506,
-    44,
-    926,
-    691,
-    568,
-    68,
-    730,
-    933,
-    737,
-    531,
-    180,
-    414,
-    751,
-    28,
-    546,
-    60,
-    371,
-    493,
-    370,
-    527,
-    387,
-    43,
-    541,
-    13,
-    457,
-    328,
-    227,
-    652,
-    365,
-    430,
-    803,
-    59,
-    858,
-    538,
-    427,
-    583,
-    368,
-    375,
-    173,
-    809,
-    896,
-    370,
-    789
-)
-private val t1 = 542
-
-private val nums2 = intArrayOf(0, 4, 3, 0)
-private val t2 = 0 // [0, 3]
+private val nums2 = intArrayOf(3, 2, 4)
+private val t2 = 6 // [1,2]
 
 fun main(args: Array<String>) {
     val result = twoSum(nums2, t2)
-    for (i: Int in result) {
-        print("$i ")
-    }
-    Arrays.toString(result)
+    result.forEach { print("$it ") }
 }
 
 private fun twoSum(nums: IntArray, target: Int): IntArray {
 
+    // key is complement
+    // value is nums index
+    val hm = mutableMapOf<Int, Int>()
+
     for ((index, value) in nums.withIndex()) {
-        val j = bSearch(index, nums.size - 1, target - value, nums)
-        if (j != -1) return@twoSum intArrayOf(index, j)
+
+        val c = target - value // complement
+        if (hm.containsKey(value)) {
+            return intArrayOf(index, hm[value] ?: -1)
+        }
+        hm[c] = index
+    }
+
+    val lastIndex = nums.size - 1
+    val lastValue = nums[lastIndex]
+
+    if (hm.containsKey(lastValue)) {
+        return intArrayOf(lastIndex, hm[lastValue] ?: -1)
     }
 
     return intArrayOf(-1, -1)
 }
-
-private fun bSearch(left: Int, right: Int, c: Int, nums: IntArray): Int {
-    var l = left
-    var r = right
-
-    while (l < r) {
-        val m = (l + r) / 2
-
-        val current = nums[m]
-        if (current == c) {
-            return m
-        } else if (current < c) {
-            l = m + 1
-        } else {
-            r = m - 1
-        }
-    }
-
-    return -1
-}
-
