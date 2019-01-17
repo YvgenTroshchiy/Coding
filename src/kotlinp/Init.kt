cd8127f8e25a4f1d3e629701bcd9d28973eb1fce
Yevhen Troshchii <yvgen.troshchiy@gmail.com>
2019-01-17 21:27:57 +0200

    Update Init.kt

--- src/kotlinp/Init.kt
+++ src/kotlinp/Init.kt
@@ -4,6 +4,7 @@ fun main(args: Array<String>) {
     val setInt = setOf<Int>()
     val setInt2 = mutableSetOf<Int>()
 
+    var intA0 = IntArray(3)
     var intA = intArrayOf(0, 1, 2, 3)
     val intA2 = arrayOf<Int>(1, 2, 3)
     val intA3 = arrayOf<Int>()
