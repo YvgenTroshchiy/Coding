879ebc4afdf738c05b2a1ed53dccbb21decc2fc2
Yevhen Troshchii <yvgen.troshchiy@gmail.com>
2019-01-23 16:49:15 +0200

    refactor P_1_TwoSum.kt

--- src/leetcode/P_1_TwoSum.kt
+++ src/leetcode/P_1_TwoSum.kt
@@ -4,36 +4,38 @@ import java.util.*
 
 
 private val nums1 = intArrayOf(2, 7, 11, 15)
-private val t1 = 9 // [0,1]
+private const val target1 = 9
+// Output: [0,1]
 
 private val nums2 = intArrayOf(3, 2, 4)
-private val t2 = 6 // [1,2]
+private const val target2 = 6
+// Output: [1,2]
 
 fun main(args: Array<String>) {
-    println(Arrays.toString(twoSum(nums1, t1)))
-    println(Arrays.toString(twoSum(nums2, t2)))
+    println(Arrays.toString(twoSum(nums1, target1)))
+    println(Arrays.toString(twoSum(nums2, target2)))
 }
 
 private fun twoSum(nums: IntArray, target: Int): IntArray {
 
     // key is complement
     // value is nums index
-    val hm = mutableMapOf<Int, Int>()
+    val map = mutableMapOf<Int, Int>()
 
     for ((index, value) in nums.withIndex()) {
 
-        val c = target - value // complement
-        if (hm.containsKey(value)) {
-            return intArrayOf(index, hm[value] ?: -1)
+        val complement = target - value
+        if (map.containsKey(value)) {
+            return intArrayOf(index, map[value] ?: -1)
         }
-        hm[c] = index
+        map[complement] = index
     }
 
     val lastIndex = nums.size - 1
     val lastValue = nums[lastIndex]
 
-    if (hm.containsKey(lastValue)) {
-        return intArrayOf(lastIndex, hm[lastValue] ?: -1)
+    if (map.containsKey(lastValue)) {
+        return intArrayOf(lastIndex, map[lastValue] ?: -1)
     }
 
     return intArrayOf(-1, -1)
