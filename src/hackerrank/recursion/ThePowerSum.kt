c50879a8e9a5706b41bb46ae06963baa157c40f5
Yevhen Troshchii <yvgen.troshchiy@gmail.com>
2019-01-23 11:42:19 +0200

    Add draft ThePowerSum

---
+++ src/hackerrank/recursion/ThePowerSum.kt
@@ -0,0 +1,18 @@
+package hackerrank.recursion
+
+//fun main(args: Array<String>) {
+//    val scan = Scanner(System.`in`)
+//    val X = scan.nextLine().trim().toInt()
+//    val N = scan.nextLine().trim().toInt()
+//    val result = powerSum(X, N)
+//    println(result)
+//}
+
+fun main(args: Array<String>) {
+
+}
+
+// Complete the powerSum function below.
+fun powerSum(X: Int, N: Int): Int {
+    return 0
+}
