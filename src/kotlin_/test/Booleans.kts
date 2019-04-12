b1dcc37b4bb2b534baf519a7609fa191a15eb151
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2019-04-12 16:10:10 +0300

    Update Booleans.kts

--- src/kotlin_/test/Booleans.kts
+++ src/kotlin_/test/Booleans.kts
@@ -2,20 +2,20 @@ package kotlin_.test
 
 fun main2() {
     var b1 = false || false && false
-    println("b1: ${b1}")
+    println("b1: $b1")
 
     var b2 = false || false && true
-    println("b2: ${b2}")
+    println("b2: $b2")
 
 
     var b3 = true || false && false
-    println("b3: ${b3}")
+    println("b3: $b3")
 
     var b4 = true || false && true
-    println("b4: ${b4}")
+    println("b4: $b4")
 
     var b5 = false || false && true
-    println("b5: ${b5}")
+    println("b5: $b5")
 }
 
 main2()
